18ad5395952c250ec2c6f64ea6e48e4fd38e8bd7
Apoorva M <125950564+ApoorvaM16@users.noreply.github.com>
2024-02-04 12:54:23 -0500

    Update 1480-running-sum-of-1d-array.py

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -1,26 +1,10 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
 
-        left = 0
-        n = len(nums)
-        # right = 1
-        
-       
-        # while right < n:
-        #     nums[right] = nums[left] + nums[right] 
-        #     right +=1
-        #     left +=1
-        # return nums
-
-        for right in range(1,n):
-            nums[right] = nums[right-1]+nums[right]
-            # left +=1
+        # Modifying nums in-place
+        for i in nums(1,len(nums)):
+            nums[i] = nums[i] + nums[i-1]
         return nums
-          
-        
-
-        
-        
         
         # prevSum, res = 0, []
         # for i in nums:
