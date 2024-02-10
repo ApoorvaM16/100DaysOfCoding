ae1c031f043f75e6917e4c51abfb125fdb0d2377
Apoorva M <125950564+ApoorvaM16@users.noreply.github.com>
2024-02-10 17:12:12 -0500

    Time: 40 ms (64.24%), Space: 16.6 MB (66.90%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,24 @@
+from math import floor
+class Solution:
+    def mySqrt(self, x: int) -> int:
+#         doing this problem using Binary Search
+# we know that the squareroot lies in the range of 0 to x and it is in sorted order
+# So we can use binary search
+
+
+        if x == 0:
+            return 0
+        elif x == 1: return 1
+        start, end,ans =0, x, -1
+        while start < end:
+            mid = start + (end - start)//2
+            midSquare = floor(mid * mid)
+            if midSquare > x:
+                end = mid
+            elif midSquare < x:
+                ans = mid
+                start = mid + 1
+            else:
+                return mid
+        return ans
+        
