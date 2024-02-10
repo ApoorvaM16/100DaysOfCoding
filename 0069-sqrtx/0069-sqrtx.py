b35eab437ea9b163d4df608db25dda7343916cf5
Apoorva M <125950564+ApoorvaM16@users.noreply.github.com>
2024-02-10 17:15:43 -0500

    Time: 34 ms (88.54%), Space: 16.5 MB (85.40%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -6,15 +6,15 @@ class Solution:
 # So we can use binary search
 
 
-        if x == 0:
-            return 0
-        elif x == 1: return 1
+        # if x == 0:
+        #     return 0
+        # elif x == 1: return 1
         start, end,ans =0, x, -1
-        while start < end:
+        while start <=end:
             mid = start + (end - start)//2
-            midSquare = floor(mid * mid)
+            midSquare = mid * mid
             if midSquare > x:
-                end = mid
+                end = mid - 1
             elif midSquare < x:
                 ans = mid
                 start = mid + 1
