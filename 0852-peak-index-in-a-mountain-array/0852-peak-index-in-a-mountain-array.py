99af6d5780f716376f19c222de62f6b62942bd51
Apoorva M <125950564+ApoorvaM16@users.noreply.github.com>
2024-02-10 14:04:26 -0500

    Time: 459 ms (70.75%), Space: 30.3 MB (96.71%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -1,9 +1,17 @@
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
     # O(n) TC: better approach, may not pass some testcases
-        return arr.index(max(arr))      
+        # return arr.index(max(arr))      
     
     # O(logn) TC : Optimal approach
 
-
+        start, end = 0, len(arr) - 1
+        while start < end :
+            mid = start + (end - start)//2
+            if arr[mid] < arr[mid + 1]:
+                start = mid + 1
+            else:
+                end = mid
+            
+        return start
         
