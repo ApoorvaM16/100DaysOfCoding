8bb14523a3bd7c5ab5b5c1a35a2cfc2357839128
Apoorva M <125950564+ApoorvaM16@users.noreply.github.com>
2024-02-10 17:25:45 -0500

    Time: 32 ms (92.40%), Space: 16.7 MB (52.89%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -7,29 +7,17 @@ class Solution:
     # they said not to use this  :)
         # return int(x ** 0.5)
       
-        # start, end, ans =0, x, -1
-        # while start <=end:
-        #     mid = start + (end - start)//2
-        #     midSquare = mid * mid
-        #     if midSquare > x:
-        #         end = mid - 1
-        #     elif midSquare < x:
-        #         # ans = mid
-        #         start = mid + 1
-        #     else:
-        #         return mid
-        # return ans
-        
-        left, right = 0,  x
-        while left <= right:
-            mid = left + (right - left) // 2
-
-            if mid * mid > x:
-                right = mid - 1
-            elif mid * mid < x:
-                left = mid + 1
+        start, end =0, x
+        while start <=end:
+            mid = start + (end - start)//2
+            midSquare = mid * mid
+            if midSquare > x:
+                end = mid - 1
+            elif midSquare < x:
+                # ans = mid
+                start = mid + 1
             else:
                 return mid
-
-        # left > right
-        return right
+        return end
+        
+        
