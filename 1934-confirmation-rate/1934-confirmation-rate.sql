732a55b9e1e81380bb6dfaefcb7af63ddfc9e184
Apoorva M <125950564+ApoorvaM16@users.noreply.github.com>
2024-03-03 17:51:28 -0500

    Time: 1235 ms (55.68%), Space: 0B (100.00%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -2,12 +2,24 @@
 # select round(/),2) from confirmations 
 
 
-select s.user_id, coalesce(round(c1.confirmed / c.total,2),0.00) confirmation_rate
-from signups s left join
-(select user_id,count(*) total from confirmations group by user_id) c on s.user_id = c.user_id 
-left join 
-(
-    select user_id,count(*) confirmed from confirmations where action = 'confirmed' group by user_id
-) c1 on s.user_id = c1.user_id
+# select s.user_id, coalesce(round(c1.confirmed / c.total,2),0.00) confirmation_rate
+# from signups s left join
+# (select user_id,count(*) total from confirmations group by user_id) c on s.user_id = c.user_id 
+# left join 
+# (
+#     select user_id,count(*) confirmed from confirmations where action = 'confirmed' group by user_id
+# ) c1 on s.user_id = c1.user_id
+
+
+# select s.user_id, round(avg(if(c.action="confirmed",1,0)),2) as confirmation_rate
+# from Signups as s left join Confirmations as c on s.user_id= c.user_id group by user_id;
+
+
+SELECT S.USER_ID, ROUND(AVG(IF(C. ACTION = 'CONFIRMED',1,0)),2) CONFIRMATION_RATE 
+FROM SIGNUPS S LEFT JOIN CONFIRMATIONS C ON S.USER_ID = C.USER_ID GROUP BY USER_ID
+
+
+
+
 
 
