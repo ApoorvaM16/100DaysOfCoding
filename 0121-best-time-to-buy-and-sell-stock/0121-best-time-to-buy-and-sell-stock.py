51971b0e406aa1918038aceaa4aab7a6faf0ed1c
Apoorva M <125950564+ApoorvaM16@users.noreply.github.com>
2024-03-30 14:08:39 -0400

    Time: 826 ms (7.13%), Space: 27.4 MB (72.44%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,80 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-
-        # kadane's
+        currProfit, maxProfit = float('inf'),0
+        for i in range(len(prices)):
+            currProfit = min(currProfit, prices[i])
+            maxProfit = max(prices[i]-currProfit, maxProfit)
+        return maxProfit
         
-
-
-        # modified kadane's
-        # maxprofit = 0
-        # minbuy = prices[0]
-        
-        # for i in range(1, len(prices)):
-        #     minbuy = min(minbuy, prices[i])
-        #     maxprofit = max(maxprofit, prices[i]-minbuy)
-        # return maxprofit
-
-
-        # two pointer
-        l, r = 0, 1
-        # maxprofit = 0
-
-        # while r<len(prices):
-        #     if prices[l] < prices[r]:
-        #         maxprofit = max(maxprofit, prices[r] - prices[l])
-            
-        #     else:
-        #         l = r
-        #     r +=1
-        # return maxprofit
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        currMin, maxSoFar = float('inf'), 0
-        for price in prices:
-            currMin = min(currMin, price)
-            maxSoFar = max(maxSoFar, price - currMin)
-        return maxSoFar
-
-
-
-
-
-         
