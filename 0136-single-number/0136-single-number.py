c65c74aa75fc14cb962a462a24880c682a2864a5
Apoorva M <125950564+ApoorvaM16@users.noreply.github.com>
2024-03-31 17:38:23 -0400

    Time: 110 ms (62.65%), Space: 19.2 MB (19.57%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,21 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-    # Using extra space  O(n) TC and O(n) SC
-        # dic = {}
-        # for i in nums:
-        #     if i in dic:
-        #         dic[i] +=1
-        #     else:
-        #         dic[i] = 1
-        # for key, val in dic.items():
-        #     if val ==1:
-        #         return key
-
-# O(n) TC O(1) SC
-        xor = 0
+        num_map = defaultdict(int)
         for i in nums:
-            xor ^=i
-        return xor
-
-
+            num_map[i] +=1
+        for key, val in num_map.items():
+            if val <2:
+                return key
         
