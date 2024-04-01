ab51780041268d3f28d939f8fb2a15f1e746b0fc
Apoorva M <125950564+ApoorvaM16@users.noreply.github.com>
2024-03-31 22:53:12 -0400

    Time: 1011 ms (64.88%), Space: 48.7 MB (5.69%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,104 +1,15 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-#         even, odd = 0, 1
-#         n = len(nums)
-#         res = [0] *n 
-#         for i in nums:
-#             if i> 0:
-#                 res[even] = i
-#                 even +=2
-#             if i < 0:
-#                 res[odd] = i
-#                 odd +=2
-                     
-#         return res
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        even, odd, res = [],[], []
-        for i in nums:
-            if i > 0:
-                even.append(i)
+        negative, positive,res = [],[],[]
+        for i in range(len(nums)):
+            if nums[i] < 0:
+                negative.append(nums[i])
             else:
-                odd. append(i)
-        for i in range(len(even)):
-            res.append(even[i])
-            res.append(odd[i])
+                positive.append(nums[i])
+        for i in range(len(positive)):
+            res.append(positive[i])
+            res.append(negative[i])
         return res
-
-
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
         
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+                
         
