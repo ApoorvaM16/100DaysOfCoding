1d2cb6293bf56fdbe403fd121ae6e7ade374315a
Apoorva M <125950564+ApoorvaM16@users.noreply.github.com>
2024-04-02 12:27:32 -0400

    Time: 226 ms (68.49%), Space: 19.4 MB (74.16%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,62 +1,25 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        
-#         prefix, res, dic = 0, 0, {0:1}
-       
-#         for i in nums:
-#             prefix +=i
-
-#             if prefix-k in dic:
-#                 res +=dic[prefix-k]
-
-#             dic[prefix] = dic.get(prefix,0) +1  
-#         return res
-
-
-        prefix,res, dic = 0, 0, {0:1}
-        for i in range(len(nums)):
-            prefix +=nums[i]
+        prefix, numMap, res = 0, {0: 1}, 0
+        for i in nums:
+            prefix +=i
             
-            if prefix - k in dic:
-                res += dic[prefix-k]
-            dic[prefix] = dic.get(prefix, 0) + 1
+            if prefix - k in numMap:
+                res += numMap[prefix-k]
+            if prefix in numMap:
+                numMap[prefix] +=1
+            else:
+                numMap[prefix] = 1
         return res
-
-
-
-
                 
-#         Bruteforce
-        # count = 0
-        # for i in range(len(nums)):
-        #     prefix = 0
-        #     for j in range(i,len(nums)):
-        #         prefix +=nums[j]
-        #         if prefix == k:
-        #             count +=1
-        # return count
-                
-# Hashmap/Dictionary
         
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+        
+#         prefix,res, dic = 0, 0, {0:1}
+#         for i in range(len(nums)):
+#             prefix +=nums[i]
+            
+#             if prefix - k in dic:
+#                 res += dic[prefix-k]
+#             dic[prefix] = dic.get(prefix, 0) + 1
+#         return res
+        
