2a0a3cf889c0627f12b5c8c3f6f8912bd5e2994c
Apoorva M <125950564+ApoorvaM16@users.noreply.github.com>
2024-04-01 23:18:48 -0400

    Time: 52 ms (5.74%), Space: 16.5 MB (52.83%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -3,51 +3,41 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # find the dip in the peak from backside
-        # index = -1
-        # n = len(nums)
-        # for i in range(n-2,-1,-1):
-        #     if nums[i] < nums[i+1]:
-        #         index = i
-        #         break
-        # if index == -1:
-        #     nums.reverse()
-        #     return
-        
-        # # Swap next smallest
-        # for i in range(n-1,-1,-1):
-        #     if nums[i] > nums[index]:
-        #         nums[i], nums[index] = nums[index], nums[i]
-        #         break
-         
-        # #  sort the subarray
-        # nums[index+1:] = nums[index +1:][::-1]
-
-
-# practicing again
-       
-        index, n = -1, len(nums)
-    # searching for the dip
-        for i in range(n-2, -1, -1):
-            if nums[i] < nums[i+1]:
-                index = i
+        n, index = len(nums), -1
+        # finding the dip
+        for i in range(n-1,-1,-1):
+            if nums[i] > nums[i-1]:
+                # nums[i], nums[i-1] = nums[i-1], nums[i]
+                index = i-1
                 break
         if index == -1:
             nums.reverse()
             return
-    # dip found, swap with next smallest number
+        # dip found, swapping to get the least next element
         for i in range(n-1,-1,-1):
             if nums[i] > nums[index]:
-               nums[i], nums[index] = nums[index], nums[i]
-               break
-    # sort the substring
-        nums[index + 1: ] = nums[index + 1: ][::-1]
+                nums[i], nums[index] = nums[index], nums[i]
+                break
+            
+        # sorting to get the next permutation
+        nums[index+1:] = nums[index+1:][::-1]
         
-
-
-
-
-
-
-
+        
+        
+#           index, n = -1, len(nums)
+#     # searching for the dip
+#         for i in range(n-2, -1, -1):
+#             if nums[i] < nums[i+1]:
+#                 index = i
+#                 break
+#         if index == -1:
+#             nums.reverse()
+#             return
+#     # dip found, swap with next smallest number
+#         for i in range(n-1,-1,-1):
+#             if nums[i] > nums[index]:
+#                nums[i], nums[index] = nums[index], nums[i]
+#                break
+#     # sort the substring
+#         nums[index + 1: ] = nums[index + 1: ][::-1]
         
