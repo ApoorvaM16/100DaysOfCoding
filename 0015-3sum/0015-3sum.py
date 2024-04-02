6c264f5cb623000db4c954a567b91bbe6b7b691c
Apoorva M <125950564+ApoorvaM16@users.noreply.github.com>
2024-04-02 13:04:34 -0400

    Time: 705 ms (57.98%), Space: 20.8 MB (26.04%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,22 +1,52 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        n, res = len(nums),[]
-        nums.sort()
+#         n, res = len(nums),[]
+#         nums.sort()
         
-        for i in range(n):
-            if i >0 and nums[i] == nums[i-1]: continue
-            j,k = i+1, n-1
-            while j < k:
+#         for i in range(n):
+#             if i >0 and nums[i] == nums[i-1]: continue
+#             j,k = i+1, n-1
+#             while j < k:
                                
-                tot = nums[i]+nums[j]+nums[k]
+#                 tot = nums[i]+nums[j]+nums[k]
+#                 if tot == 0:
+#                     res.append([nums[i],nums[j],nums[k]])
+#                     j +=1
+#                     k -=1
+#                     while j <k and nums[j] == nums[j-1]: j +=1
+#                     while j<k and nums[k] == nums[k+1]: k-=1
+#                 elif tot < 0:
+#                      j +=1
+#                 else:
+#                     k -= 1
+#         return res
+
+
+
+
+
+
+
+
+
+        nums.sort()
+        n,res = len(nums), []
+        for i in range(n):
+            if i>0 and nums[i] == nums[i-1]: continue
+            j = i+1
+            k = n-1
+            while j < k:
+                tot = nums[i] + nums[j] + nums[k]
                 if tot == 0:
-                    res.append([nums[i],nums[j],nums[k]])
-                    j +=1
+                    res.append([nums[i], nums[j], nums[k]])
+                    j+=1
+                    k-=1
+                    while j < k and nums[j] == nums[j-1]: j +=1
+                    while j < k and nums[k] == nums[k+1]: k -=1
+                elif tot > 0:
                     k -=1
-                    while j <k and nums[j] == nums[j-1]: j +=1
-                    while j<k and nums[k] == nums[k+1]: k-=1
-                elif tot < 0:
-                     j +=1
                 else:
-                    k -= 1
+                    j +=1
+                
         return res
+
