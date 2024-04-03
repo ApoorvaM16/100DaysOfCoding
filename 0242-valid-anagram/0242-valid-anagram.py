f9ce47e432d98db0d87f46ba4da986a5588abf56
Apoorva M <125950564+ApoorvaM16@users.noreply.github.com>
2024-04-03 17:09:28 -0400

    Time: 55 ms (28.80%), Space: 17.8 MB (13.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,11 +1,29 @@
-from collections import Counter
+# from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        # s = s.lower()
+        # t = t.lower()
+        # if Counter(t) == Counter(s):
+        #     return True
+        # return False
+        
+#         if len(s) != len(t):
+#             return False
+        
+#         freq = defaultdict(int)
+#         for i in range(len(s)):
+#             freq[s[i]] +=1
+#             freq[t[i]] -=1
+            
+#         for val in freq.values():
+#             if val != 0:
+#                 return False
+#         return True
+    
+    
         s = s.lower()
         t = t.lower()
-        if Counter(t) == Counter(s):
-            return True
-        return False
+        return sorted(s) == sorted(t)
         
         
         
