ee09338f5f025659dbe2c209e0d1260152309a26
Apoorva M <125950564+ApoorvaM16@users.noreply.github.com>
2024-04-02 21:44:15 -0400

    Time: 39 ms (52.42%), Space: 16.4 MB (97.04%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.py
@@ -0,0 +1,38 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        """
+        We can rotate an array by:
+        1. findig Transpose of a matrix
+        2. Reversing every row of a matrix
+        """
+        
+#         finding the transpose
+        n = len(matrix)
+        for i in range(n-1):
+            for j in range(i+1,n):
+                matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
+        
+        # reversing the matrix row
+        for i in matrix:
+            i[:] = i[::-1]
+            
+            
+            
+# from typing import List
+# def rotate(matrix: List[List[int]]) -> None:
+#     n = len(matrix)
+#     # transposing the matrix
+#     for i in range(n):
+#         for j in range(i):
+#             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+#     # reversing each row of the matrix
+#     for i in range(n):
+#         matrix[i].reverse()
+
+
+
+
+
