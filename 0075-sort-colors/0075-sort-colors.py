59c361c7dda87a065a5ba85f39846039b5b219ca
Apoorva M <125950564+ApoorvaM16@users.noreply.github.com>
2024-04-02 23:45:11 -0400

    Time: 38 ms (52.08%), Space: 16.6 MB (42.22%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,34 +4,20 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
-        
-        n = len(nums)
-        p1,p,p2 = 0,0,n-1
-        while p <= p2:
-            if nums[p] < 1:
-                nums[p], nums[p1] = nums[p1], nums[p]
-                p1 +=1
-                p +=1
-            elif nums[p] == 1:
-                p +=1
-                # continue
-            else:
-                nums[p], nums[p2] = nums[p2], nums[p]
-                p2 -=1
-                # p +=1
+    
                              
         
         
-        # n = len(nums)
-        # p1,p,p2 = 0,0,n-1                       #p1:red, p:white, p2:blue
-        # while p <= p2:
-        #     if nums[p] == 0:
-        #         nums[p], nums[p1] = nums[p1], nums[p]
-        #         p +=1
-        #         p1 +=1
-        #     elif nums[p] ==1:
-        #         p +=1
-        #     else:
-        #         nums[p], nums[p2] = nums[p2], nums[p]
-        #         p2 -=1
+        n = len(nums)
+        p1,p,p2 = 0,0,n-1                       #p1:red, p:white, p2:blue
+        while p <= p2:
+            if nums[p] == 0:
+                nums[p], nums[p1] = nums[p1], nums[p]
+                p +=1
+                p1 +=1
+            elif nums[p] ==1:
+                p +=1
+            else:
+                nums[p], nums[p2] = nums[p2], nums[p]
+                p2 -=1
                 
