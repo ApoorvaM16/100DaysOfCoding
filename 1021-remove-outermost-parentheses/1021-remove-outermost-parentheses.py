a6b81631c3f4e6e7ef9335f6cdcd85fd7c6378b8
Apoorva M <125950564+ApoorvaM16@users.noreply.github.com>
2024-04-11 14:09:26 -0400

    Time: 49 ms (6.05%), Space: 16.7 MB (25.17%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
@@ -1,72 +1,36 @@
 class Solution:
     def removeOuterParentheses(self, s: str) -> str:
-#         res,count = [],0
-#         for i in range(len(s)):
-#             if s[i] == '(':
-#                 res.append(s[i])
-#                 count +=1
-#             else:
-#                 res.pop()
-                
-#                 if res and count > 1:
-#                     # count -=1
-#                     res.append(s[i])
-# #                 count -=1
-# #         return "".join(res)
-                
         
-#         res, count = [], 0
-#         for c in S:
-#             if c == '(' and count > 0:
-#                 res.append(c)
-#             if c == ')' and count > 1:
-#                 res.append(c)
-#             if c == '(':
-#                 count += 1
-#             else:
-#                 count-=1
-#         return "".join(res)
-        
-        
-        
-        res, count = [], 0
-        for c in s:
-            if c == "(":
-                if count > 0:
-                    res.append(c)
+        count,res = 0, ""
+        for i in s:
+            if i == '(':
+                if count >0:
+                    res +=i
                 count +=1
             else:
-                count -=1
+                count -= 1
                 if count > 0:
-                    res.append(c)
-        return ''.join(res)
+                    res +=i
+        return res
+                    
         
         
         
+    
         
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-      
 #         res, count = [], 0
-#         for char in s:
-#             if char == '(':
+#         for c in s:
+#             if c == "(":
 #                 if count > 0:
-#                     res.append(char)
-#                 count += 1
+#                     res.append(c)
+#                 count +=1
 #             else:
-#                 count -= 1
+#                 count -=1
 #                 if count > 0:
-#                     res.append(char)
+#                     res.append(c)
 #         return ''.join(res)
+        
+        
+
