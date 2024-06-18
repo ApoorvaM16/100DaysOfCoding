aadc65ba07b3eb9aa58faaa15164a75c829d985a
Apoorva M <125950564+ApoorvaM16@users.noreply.github.com>
2024-06-18 13:08:21 -0400

    Time: 130 ms (18.16%), Space: 18.1 MB (13.60%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,22 +1,11 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-    #O(n) TC and O(n) SC
-        # numMap = defaultdict(int)
-        # for num in nums:
-        #     numMap[num] += 1
-        # for i in range(len(nums)+1):
-        #     if i not in numMap:
-        #         return i
-        # return -1
-
-
-
-
-        # maths broo    O(n) TC and O(1) SC
-        numSum = sum(nums)
-        total = sum([i for i in range(1,len(nums)+1)])
-        return total-numSum
-
-
-
-
+        numMap = {}
+        for i in nums:
+            numMap[i] = 1
+        for i in range(len(nums)+1):
+            if i not in numMap.keys():
+                return i
+                
+            
+        
