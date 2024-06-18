6b4c9c6c6052aff56c3be0f4fd77dedd57408f0e
Apoorva M <125950564+ApoorvaM16@users.noreply.github.com>
2024-06-18 13:11:41 -0400

    Time: 171 ms (16.59%), Space: 17.7 MB (59.69%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,11 +1,16 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        numMap = {}
-        for i in nums:
-            numMap[i] = 1
-        for i in range(len(nums)+1):
-            if i not in numMap.keys():
-                return i
+        # numMap = {}
+        # for i in nums:
+        #     numMap[i] = 1
+        # for i in range(len(nums)+1):
+        #     if i not in numMap.keys():
+        #         return i
+        
+#         using math
+        numSum = sum(nums)
+        total = sum([i for i in range(len(nums)+1)])
+        return total - numSum
                 
             
         
