c4912d1f0529147da61a85d525f12c80ad469eac
Apoorva M <125950564+ApoorvaM16@users.noreply.github.com>
2024-06-18 14:21:48 -0400

    Time: 810 ms (36.18%), Space: 20.7 MB (67.53%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,52 +1,22 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-#         n, res = len(nums),[]
-#         nums.sort()
-        
-#         for i in range(n):
-#             if i >0 and nums[i] == nums[i-1]: continue
-#             j,k = i+1, n-1
-#             while j < k:
-                               
-#                 tot = nums[i]+nums[j]+nums[k]
-#                 if tot == 0:
-#                     res.append([nums[i],nums[j],nums[k]])
-#                     j +=1
-#                     k -=1
-#                     while j <k and nums[j] == nums[j-1]: j +=1
-#                     while j<k and nums[k] == nums[k+1]: k-=1
-#                 elif tot < 0:
-#                      j +=1
-#                 else:
-#                     k -= 1
-#         return res
-
-
-
-
-
-
-
-
-
         nums.sort()
-        n,res = len(nums), []
+        n, res = len(nums), []
         for i in range(n):
-            if i>0 and nums[i] == nums[i-1]: continue
-            j = i+1
-            k = n-1
+            if i > 0  and nums[i] == nums[i-1]: continue
+            j, k = i + 1, n -1
             while j < k:
-                tot = nums[i] + nums[j] + nums[k]
-                if tot == 0:
+                temp = nums[i] + nums[j] + nums[k]
+                if temp == 0:
                     res.append([nums[i], nums[j], nums[k]])
-                    j+=1
-                    k-=1
-                    while j < k and nums[j] == nums[j-1]: j +=1
-                    while j < k and nums[k] == nums[k+1]: k -=1
-                elif tot > 0:
+                    j +=1
                     k -=1
+                    while nums[j] == nums[j-1] and j < k: j +=1
+                    while nums[k] == nums[k+1] and j< k: k -=1
+                elif temp > 0:
+                    k -= 1
                 else:
                     j +=1
-                
+            
         return res
-
+    
