f43d28befa457f45e65753a56778f612478c149c
Apoorva M <125950564+ApoorvaM16@users.noreply.github.com>
2024-06-21 18:20:52 -0400

    Time: 173 ms (46.39%), Space: 17.9 MB (61.34%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,26 +1,13 @@
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # num_map = {}
-        # for i in nums:
-        #     if i not in num_map:
-        #         num_map[i] = 1
-        #     else:
-        #         num_map[i] +=1
-        #         if num_map[i] > len(nums)//2:
-        #             return i
-        # return i
-    
-    # moore algorithm
-        count, candidate = 0,0
-        for num in nums:
-            if count == 0:
-                candidate = num
+        numMap, n = defaultdict(int), len(nums)
+        for i in nums:
+            numMap[i] += 1
+        for key, val in numMap.items():
+            if val > n//2:
+                return key
             
-            if num == candidate:
-                count +=1
-            else:
-                count -=1
-        return candidate
-                
-         
+            
+            
         
