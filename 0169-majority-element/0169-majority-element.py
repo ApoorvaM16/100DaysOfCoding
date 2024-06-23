7799608bec75fe7914e919c19b64b662cc194807
Apoorva M <125950564+ApoorvaM16@users.noreply.github.com>
2024-06-23 12:16:56 -0400

    Time: 159 ms (94.43%), Space: 17.8 MB (89.71%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,12 +1,24 @@
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        numMap, n = defaultdict(int), len(nums)
+        # linear time, linear space
+        # numMap, n = defaultdict(int), len(nums)
+        # for i in nums:
+        #     numMap[i] += 1
+        # for key, val in numMap.items():
+        #     if val > n//2:
+        #         return key
+        
+#         linear time, constant space
+        candidate, count = 0,0
         for i in nums:
-            numMap[i] += 1
-        for key, val in numMap.items():
-            if val > n//2:
-                return key
+            if count == 0:
+                candidate = i
+            if i == candidate:
+                count +=1
+            else:
+                count -=1
+        return candidate
             
             
             
