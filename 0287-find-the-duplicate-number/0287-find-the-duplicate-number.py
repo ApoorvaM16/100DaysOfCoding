e133e5012ac5ef3a1ade5e6978b06e8c4dab89cd
Apoorva M <125950564+ApoorvaM16@users.noreply.github.com>
2024-06-26 19:50:22 -0400

    Time: 490 ms (25.92%), Space: 38.4 MB (5.22%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,48 +1,9 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-# Practising the slow-fast pointer approach one more time
-        slow = fast = nums[0]
-        while True:
-            slow = nums[slow]
-            fast = nums[nums[fast]]
-            if slow == fast:
-                break
-        slow = nums[0]
-        while slow != fast:
-            slow = nums[slow]
-            fast = nums[fast]
-        return slow
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # slow= fast = nums[0]
-        # while True:
-        #     slow = nums[slow]
-        #     fast = nums[nums[fast]]
-        #     if slow == fast:
-        #         break
+        numMap = defaultdict(int)
+        for i in nums:
+            numMap[i] +=1
+        for key,val in numMap.items():
+            if val > 1:
+                return key
         
-        # slow = nums[0]
-        # while slow != fast:
-        #     slow = nums[slow]
-        #     fast = nums[fast]
-        # return slow
