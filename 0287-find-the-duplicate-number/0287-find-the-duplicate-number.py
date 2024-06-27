9e9a73409483aa79d31488edf9941decbe546d7f
Apoorva M <125950564+ApoorvaM16@users.noreply.github.com>
2024-06-26 21:48:54 -0400

    Time: 465 ms (61.04%), Space: 30.4 MB (70.27%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -11,14 +11,14 @@ class Solution:
 #     constant space: using slow fast pointer
         slow = fast = nums[0]
         while True:
-            slow = nums[slow]
-            fast = nums[nums[fast]]
+            slow = nums[slow]                  # 1 step
+            fast = nums[nums[fast]]           #2 steps
             if slow == fast:
                 break              #we have found the cycle, that tells duplicates are there
                 
 #         finding start of the cycle
         slow = nums[0]
         while slow != fast:
-            slow = nums[slow]
-            fast = nums[fast]
+            slow = nums[slow]           # 1 step to find the start
+            fast = nums[fast]            # 1 step to find the start
         return slow
