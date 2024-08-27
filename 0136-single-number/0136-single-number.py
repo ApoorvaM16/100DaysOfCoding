797372f49bc90ca06e5a60d9c3fcb6c1b5725872
Apoorva M <125950564+ApoorvaM16@users.noreply.github.com>
2024-08-27 15:34:09 -0400

    Time: 101 ms (93%), Space: 19 MB (54.49%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,20 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-#         using dictionary
-        # numMap = {}
-        # for i in nums:
-        #     if i in numMap:
-        #         numMap[i] +=1
-        #     else:
-        #         numMap[i] = 1
-        # for key, val in numMap.items():
-        #     if val == 1:
-        #         return key
-            
-        
-        xor = 0;
+        xor = 0
         for i in nums:
-            xor ^=i;
+            xor ^= i
         return xor
-            
         
