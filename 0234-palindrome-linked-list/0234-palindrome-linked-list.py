411b7358e13303911b1ce4abe56c94731c9b6fd4
Apoorva M <125950564+ApoorvaM16@users.noreply.github.com>
2024-10-20 19:05:05 -0400

    Time: 111 ms (97.36%), Space: 43.9 MB (5.01%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -4,10 +4,46 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+     # 2. reverse sencond part
+    
+
+
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        current, arr = head, []
-        while current:
-            arr.append(current.val)
-            current = current.next
-        return arr == arr[::-1]
+        # optimal O(1), O(n)
+        # 1. Find the middle
+        slow = fast = head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        def reverseLL(temp):
+            if temp is None or temp.next is None:
+                return temp
+            newHead = reverseLL(temp.next)
+            front = temp.next
+            front.next = temp
+            temp.next = None
+            return newHead
+
+        newHead = reverseLL(slow.next)  
+
+        # 3. compare
+        first = head
+        second = newHead
+        while second:
+            if first.val != second.val:
+                reverseLL(newHead)
+                return False
+            first = first.next
+            second = second.next
+        reverseLL(newHead)
+        return True
+
+
+
+        # current, arr = head, []
+        # while current:
+        #     arr.append(current.val)
+        #     current = current.next
+        # return arr == arr[::-1]
         
