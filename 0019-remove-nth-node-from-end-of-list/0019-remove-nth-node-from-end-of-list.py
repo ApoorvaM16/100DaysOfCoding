e2a417caef053ec1f18105097cf25b6c9c99f9d2
Apoorva M <125950564+ApoorvaM16@users.noreply.github.com>
2024-10-22 11:06:16 -0400

    Time: 0 ms (100%), Space: 16.7 MB (7.49%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,28 +5,45 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        if n == 1 and head.next is None:
-            return None
-        if n == 1 and head.next.next is None:
-            head.next = None
-            return head
-        def lengthLL(head):
-            current, count = head, 0
-            while current:
-                current = current.next
-                count +=1
-
-            return count
-
-        length = lengthLL(head)
-        counter = length - n
-        if counter == 0:
+        fast = head
+        for i in range(n):
+            fast = fast.next
+            
+        if fast == None:
             return head.next
-        current = head
-        while current:
-            counter -= 1
-            if counter == 0:
-                current.next = current.next.next
-                return head
-            current = current.next
+
+        slow = head
+        while fast.next is not None:
+            fast = fast.next
+            slow = slow.next
+        
+        slow.next = slow.next.next
+        return head
+
+
+
+        # if n == 1 and head.next is None:
+        #     return None
+        # if n == 1 and head.next.next is None:
+        #     head.next = None
+        #     return head
+        # def lengthLL(head):
+        #     current, count = head, 0
+        #     while current:
+        #         current = current.next
+        #         count +=1
+
+        #     return count
+
+        # length = lengthLL(head)
+        # counter = length - n
+        # if counter == 0:
+        #     return head.next
+        # current = head
+        # while current:
+        #     counter -= 1
+        #     if counter == 0:
+        #         current.next = current.next.next
+        #         return head
+        #     current = current.next
         
